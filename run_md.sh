c496902f3629b74737a912d7365bb0ae5118c828
Mohd Athar <102149843+athar91@users.noreply.github.com>
2023-11-22 14:09:31 +0100

    Update run_md.sh

--- run_md.sh
+++ run_md.sh
@@ -9,7 +9,7 @@ PARM=$DIR/1fko_trunc_clean.prmtop
 ################
 #1
 NAME=opt_restr_all
-IN=setup_files/${NAME}.pmemd
+IN=${NAME}.pmemd
 OUT=${NAME}.out
 INITCRD=$DIR/1fko_trunc_clean.inpcrd
 RESTART=rest_${NAME}.rst7
@@ -17,12 +17,12 @@ REF=$DIR/$INITCRD
 COORD=${NAME}.nc 
 ENERGY=${NAME}.energy
 
-#$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY -ref $REF
+$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY -ref $REF
 
 ################
 #2
 NAME=opt_restr_backbone
-IN=setup_files/${NAME}.pmemd
+IN=${NAME}.pmemd
 OUT=${NAME}.out
 INITCRD=$RESTART
 REF=$INITCRD
@@ -30,12 +30,12 @@ RESTART=rest_${NAME}.rst7
 COORD=${NAME}.nc 
 ENERGY=${NAME}.energy
 
-#$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY -ref $REF
+$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY -ref $REF
 
 ################
 #3
 NAME=opt_restr_CAs
-IN=setup_files/${NAME}.pmemd
+IN=${NAME}.pmemd
 OUT=${NAME}.out
 INITCRD=$RESTART
 REF=$INITCRD
@@ -48,7 +48,7 @@ ENERGY=${NAME}.energy
 ################
 #4
 NAME=opt
-IN=setup_files/${NAME}.pmemd
+IN=${NAME}.pmemd
 OUT=${NAME}.out
 INITCRD=$RESTART
 REF=$INITCRD
@@ -56,7 +56,7 @@ RESTART=rest_${NAME}.rst7
 COORD=${NAME}.nc 
 ENERGY=${NAME}.energy
 
-#$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
+$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
 
 ################
 #5
@@ -69,13 +69,13 @@ RESTART=rest_${NAME}.rst7
 COORD=${NAME}.nc 
 ENERGY=${NAME}.energy
 
-#$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
+$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
 #mpirun -np 2 pmemd.MPI -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
 
 ################
 #6
 NAME=equilibrate_NTP
-IN=setup_files/${NAME}.pmemd
+IN=${NAME}.pmemd
 OUT=${NAME}.out
 INITCRD=$RESTART
 REF=$INITCRD
@@ -83,11 +83,11 @@ RESTART=rest_${NAME}.rst7
 COORD=${NAME}.nc 
 ENERGY=${NAME}.energy
 
-#$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
+$EXE -O -i $IN -o $OUT -p $PARM -c $INITCRD -r $RESTART -x $COORD -e $ENERGY
 #########
 #7
 NAME=md1
-IN=setup_files/${NAME}.pmemd
+IN=${NAME}.pmemd
 OUT=${NAME}.out
 INITCRD=$RESTART
 RESTART=rest_${NAME}.rst7
